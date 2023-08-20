0852196c106920bc77d198a9b0adf046d97f50d1
EtienneShamley <128038227+EtienneShamley@users.noreply.github.com>
2023-08-20 20:47:34 +1200

    axios installed and backend created

---
+++ frontend/app/lib/articleloader.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { ArticlesInterface } from "./article";
+
+export async function loadArticles(): Promise<ArticlesInterface[]> {
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/researchPapers`;
+
+  try {
+    // Making a GET request using axios
+    const response = await axios.get(apiUrl);
+    const data = response.data;
+
+    // Mapping the response to match the ArticlesInterface structure
+    const articles: ArticlesInterface[] = data.map((article: any) => ({
+      id: article.id ?? article._id,
+      title: article.title,
+      authors: article.authors.join(", "),
+      source: article.source,
+      pubyear: String(article.publicationYear),
+      doi: article.doi,
+      claim: article.claim,
+      evidence: article.evidence,
+    }));
+
+    return articles;
+  } catch (error) {
+    // Handle error here
+    console.error("Error loading articles:", error);
+    return [];
+  }
+}
